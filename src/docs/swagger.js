9e7ac012391ae0f6c92343cfde82e104d17d8cc9
alessandramacedodev <alessandramacedo.dev@gmail.com>
2025-05-31 22:03:13 -0300

    atualizando o swegger

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -11,7 +11,7 @@ const options = {
         },
         servers: [
             {
-                url: 'http://localhost:3000/'
+                url: 'https://odontoforense-backend-wur1.onrender.com/'
             },
         ],
         components: {
